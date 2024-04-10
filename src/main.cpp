ecbe6a5b6815ce17bbe89251d87c8f94d3aa6edd
Dimi <johnnnathan@proton.me>
2024-04-10 10:58:58 +0200

    made some improvements, also cursor movement and modes. Need to finish insert mode today, the only thing that is in my way is the need for a custom cursor since the library only has the invisible cursor and the block, no line

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,80 @@
-#include <cstdio>
 #include <ncurses.h>
+#include <unistd.h>
+
+
+
+int row = 0;
+int col = 0;
+int mode = 0;//should be 0 for insert and 1 for normal
+
+int max_x;
+int max_y;
+
+int changeMode();
+int moveCursor(int colDelta, int rowDelta);
 
 int main (int argc, char *argv[]) {
   int ch;
-
   initscr();
-  raw();
   keypad(stdscr, TRUE);
+  cbreak();
   noecho();
 
-  printw("Type any character to see it in bold! \n");
-  ch = getch();
-  
-  if (ch == KEY_F(1)){
-    printw("F1 Key Pressed");
+  getmaxyx(stdscr, max_y, max_x);
+  changeMode();
+  while((ch = getch()) != KEY_F(1)){
+    if (mode == 0){
+      addch(ch);
+    }
+    else{
+      switch (ch) {
+        case 'H': case 'h':
+          moveCursor(-1,0);
+          break;
+        case 'J': case'j':
+          moveCursor(0,-1);
+          break;
+        case 'K': case 'k':
+          moveCursor(0,1);
+          break;
+        case 'L': case 'l':
+          moveCursor(1,0);
+          break;
+      }
+    }
 
   }
 
-  else {
-    printw("The Key Pressed is: ");
-    attron(A_BOLD);
-    printw("%c", ch);
-    attroff(A_BOLD);
-    
-  }
-  refresh();
-  getch();
   endwin();
   return 0;
+}
+
+
+int changeMode(){
+  if (mode == 0){
+    mode = 1;
+    return 0;
+  }
+  mode = 0;
+  return mode;
+}
+
+int moveCursor(int colDelta, int rowDelta){
+  bool rowCheck = false;
+  bool colCheck = false;
+
+  if ((row + rowDelta >= 0) && (row + rowDelta <= max_x)){
+    rowCheck = true;
+  }
+  if ((col + colDelta >= 0) && (col + colDelta <= max_y)){
+    colCheck = true;
+  }
+  if (rowCheck && colCheck){
+    col = col + colDelta;
+    row = row + rowDelta;
+    move(row,col);
+    return 0;
+  }
+  return 1;
 }
 
